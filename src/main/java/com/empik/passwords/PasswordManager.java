8be4aa9eaabf46a60c98b57e59e89d67f102ffa4
Marcin Krasowski <marcin.krasowski@gmail.com>
2021-12-06 00:42:57 +0100

    building fat jar

--- src/main/java/com/empik/passwords/PasswordManager.java
+++ src/main/java/com/empik/passwords/PasswordManager.java
@@ -1,51 +1,46 @@
 package com.empik.passwords;
 
+import org.freedesktop.secret.simple.SimpleCollection;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-import java.util.concurrent.Callable;
+import java.io.IOException;
+import java.util.Scanner;
 
-@Command(name="passwords manager",mixinStandardHelpOptions = true, version = "0.0.1",
-
-        subcommands = {
-            PasswordManager.AddPassword.class,
-            PasswordManager.DeletePassword.class,
-            PasswordManager.GetPassword.class
-        }
-)
+@Command(name = "passwords manager", mixinStandardHelpOptions = true, version = "0.0.1")
 public class PasswordManager {
+
+    static Scanner scanner = new Scanner(System.in);
+    @Option(names = {"-c", "--collectionName"}, defaultValue = "Empik")
+    String collectionName;
 
     public static void main(String[] args) {
         //noinspection InstantiationOfUtilityClass
         System.exit(new CommandLine(new PasswordManager()).execute(args));
     }
 
-    @Command(name = "add")
-    static class AddPassword implements Callable<Integer>{
-        @Override
-        public Integer call() {
-            System.out.println("add password");
-            return null;
-        }
+
+    @Command(name = "add", addMethodSubcommands = false)
+    void add(
+            @Option(names = {"-l", "--label"}, interactive = true, hidden = false, required = true, prompt = "Enter label: ")
+            String label,
+            @Option(names = {"-p", "--password"}, interactive = true, hidden = false, required = true, prompt = "Enter password: ")
+            String password
+    ) throws IOException {
+        System.out.println("Enter password label: ");
+        System.out.println("Enter password: ");
+        SimpleCollection collection = new SimpleCollection(collectionName, null);
+        collection.createItem(label, new String(password));
     }
 
     @Command(name = "delete")
-    static class DeletePassword implements Callable<Integer> {
-
-        @Override
-        public Integer call() {
-            System.out.println("delete password");
-            return null;
-        }
+    void delete() {
+        System.out.println("delete password");
     }
 
     @Command(name = "get")
-    static class GetPassword implements Callable<Integer> {
-
-        @Override
-        public Integer call() {
-            System.out.println("get password");
-            return null;
-        }
+    void get() {
+        System.out.println("get password");
     }
 }
